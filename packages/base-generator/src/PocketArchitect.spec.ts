451e3b3023c29aaab331327176f0632e7771220e
Vitalii Savchuk <vitaliisavchuk@cloud.upwork.com>
2024-01-14 18:45:43 +0200

    add example

--- packages/base-generator/src/PocketArchitect.spec.ts
+++ packages/base-generator/src/PocketArchitect.spec.ts
@@ -2,8 +2,8 @@ import PocketArchitect from "./PocketArchitect";
 
 describe('PocketArchitect', () => {
   test('load', async () => {
-    const res = await PocketArchitect.load(`${__dirname}/../../../json/pocket.json5`);
-    await PocketArchitect.write(`${__dirname}/../../../json/pocket.json`, res);
+    const res = await PocketArchitect.load(`${__dirname}/../../../.pocket-architect.json5`);
+    // await PocketArchitect.write(`${__dirname}/../../../.pocket-architect.json`, res);
 
     expect(res.metadata.name).toEqual('MyApp');
     expect(res.parts[0].name).toEqual('main');
