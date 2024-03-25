62f10c0c4259c3bc2cf723f6ecf6904315e6a168
Vitalii Savchuk <vitaliisavchuk@cloud.upwork.com>
2024-03-25 09:10:53 +0200

    add type

--- packages/core/src/EntityId.ts
+++ packages/core/src/EntityId.ts
@@ -1,26 +1,27 @@
 import createUUID from 'uuid-by-string'
 import { createId } from '@paralleldrive/cuid2'
 
-export class EntityId {
-  protected _recordId: string = null;
+export class EntityId<T> {
+  protected _recordId: T|null = null;
   protected _uuid: string = null;
+  protected _hasValue: boolean = false;
 
-  constructor(recordId: string|number = null, uuid: string = null) {
-    this._recordId = recordId ? recordId.toString() : createId();
+  constructor(recordId: T = null, uuid: string = null) {
+    this._recordId = recordId ? recordId : null;
     if (!uuid) {
       this._uuid = createUUID(recordId ? recordId.toString() : createId(), createUUID(this.constructor.name));
     }
   }
 
-  toPrimitive(): string {
-    return this._recordId;
+  toPrimitive(): T {
+    return this._hasValue ? this._recordId : null;
   }
 
   toString(): string {
     return this._uuid;
   }
 
-  equals(id: EntityId): boolean {
+  equals(id: EntityId<T>): boolean {
     return this._uuid === id._uuid;
   }
 }
